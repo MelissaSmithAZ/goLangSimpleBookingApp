0a7f190fd6f7f31a25bc1db7967a8ed22b36e510
Melissa Smith <melissa_smith_arizona@yahoo.com>
2022-12-28 13:27:59 -0700

    example of helper in seperate package

--- main.go
+++ main.go
@@ -1,19 +1,72 @@
 package main
 
 import (
+	"bookinggoapp/helper"
 	"fmt"
+	"strings"
 )
 
-func main() {
-	conferenceName := "Go Conference"
-	const conferenceTickets int = 50
-	var remainingTickets uint = 50
-	var bookings []string
+const conferenceTickets int = 50
 
-	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
+var conferenceName = "Go Conference"
+var remainingTickets uint = 50
+var bookings []string
+
+func main() {
+
+	greetUsers()
+
+	for remainingTickets > 0 && len(bookings) < 50 {
+
+		firstName, lastName, email, userTickets := getUserInput()
+
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidUserInput(firstName, lastName, email, userTickets, remainingTickets)
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+
+			bookTicket(userTickets, firstName, lastName, email)
+			firstNames := getFirstNames()
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+			if remainingTickets == 0 {
+				//end program
+				fmt.Println("Our conference is  booked out.  Come back next")
+				break
+
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("first name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("email address you entered does not contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets you entered is invalid")
+			}
+		}
+	}
+
+}
+
+func greetUsers() {
+	fmt.Printf("Welcome to our %v\n", conferenceName)
 	fmt.Printf("we have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your conference tickets here to attend")
+}
 
+func getFirstNames() []string {
+
+	firstNames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	// fmt.Printf("The whole slice: %v\n", bookings)
+	// fmt.Printf("The first names of bookings are: %v\n", firstNames)
+	return firstNames
+}
+
+func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
 	var email string
@@ -27,15 +80,14 @@ func main() {
 	fmt.Scan(&email)
 	fmt.Println("Enter number of tickets you want to purchase")
 	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTickets
 	bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("The whole slice: %v\n", bookings)
-	fmt.Printf("The first value in the slice: %v\n", bookings[0])
-	fmt.Printf("slice type: %T\n", bookings)
-	fmt.Printf("slice length: %v\n", len(bookings))
-
 	fmt.Printf("Thank you %v %v for booking %v tickets.  You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for  %v\n", remainingTickets, conferenceName)
-
 }
